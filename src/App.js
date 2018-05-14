c42527cdb3577f71abe9f4d57960bffe7eacfc2c
Yerke <erke01vip@gmail.com>
2018-05-15 05:05:32 +0600

    UserForm input username added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import UserForm from './components/UserForm/UserForm'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          
-      </div>
+      <UserForm />
     );
   }
 }
--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -4,7 +4,10 @@ import React from 'react'
 const UserForm = () => {
   return (
     <div>
-      
+      <form>
+        <input type="text" name="username"/>
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
